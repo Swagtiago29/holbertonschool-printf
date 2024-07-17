bdc4c7e5ac10b6af0c2115bc53177b8ed8118c5b
sallebruno <9392@holbertonstudents.com>
2024-07-17 18:40:12 +0000

    corrected typo

--- _printf.c
+++ _printf.c
@@ -4,46 +4,47 @@
 
 int _printf(const char *format, ...)
 {
-int i, o, counter = 0;
-va_list args;
-char *str;
-va_start(args, format);
+    int i, o, counter = 0;
+    va_list args;
+    char *str;
+    va_start(args, format);
 
-for (i = 0; format[i] != '\0'; i++)
-{
-	if (format[i] == '%')
-		switch (format[i + 1])
-		{
-			case 'c': 
-          			i++;
-				_putchar(va_arg(args, int));
-				counter++;
-				break;
-			case 's':
-				i++;
-				str = (va_arg(args, char *));
-				if (str == NULL)
-				{
-					write(1, "(null)", 6) && counter += 4;
-					break;
-				}
-				for (o = 0; str[o] != '\0'; o++)
-				{	
-					_putchar(str[o]);
-					counter++;
-				}	
-				break;
-			case '%':
-				_putchar('%');
-				counter++;
-				break;
-		}
-	else
-	{
-      		_putchar(format[i]);
-		counter++;
-	}
-}
-va_end(args);
-return(counter);
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] == '%')
+            switch (format[i + 1])
+            {
+                case 'c': 
+                    i++;
+                    _putchar(va_arg(args, int));
+                    counter++;
+                    break;
+                case 's':
+                    i++;
+                    str = (va_arg(args, char *));
+                    if (str == NULL)
+                    {
+                        write(1, "(null)", 6);
+                        counter += 6;
+                        break;
+                    }
+                    for (o = 0; str[o] != '\0'; o++)
+                    {   
+                        _putchar(str[o]);
+                        counter++;
+                    }   
+                    break;
+                case '%':
+                    _putchar('%');
+                    counter++;
+                    break;
+            }
+        else
+        {
+            _putchar(format[i]);
+            counter++;
+        }
+    }
+    va_end(args);
+    return(counter);
 }
