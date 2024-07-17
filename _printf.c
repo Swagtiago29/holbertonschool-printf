9ac35a4c7e65d6733685ba820d30ca4d5e9603c2
Swagtiago29 <sant29ramos@gmail.com>
2024-07-17 16:29:30 +0000

    corrected return error

--- _printf.c
+++ _printf.c
@@ -9,7 +9,7 @@ int i;
 int o;
 va_list args;
 char *str;
-
+int counter = 0;
 va_start(args, format);
 
 for (i = 0; format[i] != '\0'; i++)
@@ -20,19 +20,30 @@ for (i = 0; format[i] != '\0'; i++)
 			case 'c': 
           			i++;
 				_putchar(va_arg(args, int));
+				counter++;
 				break;
 			case 's':
 				i++;
 				str = (va_arg(args, char *));
 				for (o = 0; str[o] != '\0'; o++)
+				{	
 					_putchar(str[o]);
+					counter++;
+				}	
 				break;
 			case '%':
+				{
 				_putchar('%');
+				counter++;
+				}
 				break;
 		}
 	else
+	{
       		_putchar(format[i]);
+		counter++;
+	}
 }
-return(i);
+va_end(args);
+return(counter);
 }
